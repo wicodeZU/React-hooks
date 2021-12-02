13748ecadc6c9c8751c07052e8df7f8987ff4b2c
serahshani <kerenketepela@gmail.com>
2021-12-02 17:03:08 +0300

    initial counter

---
+++ src/components/OnlineOfline.js
@@ -0,0 +1,17 @@
+// import React, { useState, useEffect } from "react";
+
+// const OnlineOfline = () => {
+//   const [isOnline, setIsOnline] = useState(null);
+//   useEffect(() => {
+//     // const handleStatusChange = (status) => {
+//     //   setIsOnline(status.isOnline);
+//     // };
+//     // ChatAPI.subscribeToFriendStatus(friendID, handleStatusChange);
+//     // return () => {
+//     //   ChatAPI.unsubscribeFromFriendStatus(friendID, handleStatusChange);
+//     // };
+//   });
+//   return isOnline;
+// };
+
+// export default OnlineOfline;
